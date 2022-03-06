1202a4eb9a435c345d8c104b1a6f296908104b13
KhadkeYash <90309806+KhadkeYash@users.noreply.github.com>
2022-03-06 14:42:48 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>This To Do's List App is made on React</p>
+        </div>
+    )
+}
